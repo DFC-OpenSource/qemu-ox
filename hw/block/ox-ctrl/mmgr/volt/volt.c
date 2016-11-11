926504e154604a9fe2c6a97929be4eb214e073c7
Ivan Picoli <ivpi@itu.dk>
2016-11-11 02:05:05 +0100

    Volt Media Manager single queue

--- hw/block/ox-ctrl/mmgr/volt/volt.c
+++ hw/block/ox-ctrl/mmgr/volt/volt.c
@@ -1,38 +1,226 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <mqueue.h>
 #include "volt.h"
 #include "hw/block/ox-ctrl/include/ssd.h"
 
-void nvme_volt_main(void *opaque)
+static VoltCtrl *volt;
+static struct nvm_mmgr volt_mmgr;
+
+static VoltBlock *volt_get_block(struct nvm_ppa_addr addr){
+    return volt->luns[addr.g.lun].blk_offset + addr.g.blk;
+}
+
+static size_t volt_add_mem(int64_t bytes)
 {
-    LnvmVoltCtrl *volt = (LnvmVoltCtrl *) opaque;
+    volt->status.allocated_memory += bytes;
+    return bytes;
+}
 
-    if (volt->status.active) {
-        printf("\nvolt: I am alive in ns%d, I yield here each 10 minutes!\n",volt->ns->id);
-        timer_mod(volt->mainTimer, qemu_clock_get_us(QEMU_CLOCK_VIRTUAL) + LNVM_VOLT_SECOND * 600);
+static void volt_sub_mem(int64_t bytes)
+{
+    volt->status.allocated_memory -= bytes;
+}
+
+static VoltPage *volt_init_page(VoltPage *pg)
+{
+    pg->state = 0;
+    return ++pg;
+}
+
+static int volt_init_blocks(void)
+{
+    struct nvm_mmgr_geometry *geo = volt_mmgr.geometry;
+    
+    int page_count = 0;
+    int i_blk;
+    int i_pg;
+    int total_blk = geo->n_of_planes * geo->blk_per_lun * geo->lun_per_ch * 
+                                                                   geo->n_of_ch;
+
+    volt->blocks = g_malloc(volt_add_mem (sizeof(VoltBlock) * total_blk));
+    if (!volt->blocks)
+        return VOLT_MEM_ERROR;
+               
+    for (i_blk = 0; i_blk < total_blk; i_blk++) {
+        VoltBlock *blk = &volt->blocks[i_blk];
+        blk->id = i_blk;
+        blk->life = VOLT_BLK_LIFE;
+
+        blk->pages = g_malloc(volt_add_mem(sizeof(VoltPage) * geo->pg_per_blk));
+        if (!blk->pages)
+            return VOLT_MEM_ERROR;
+        
+        blk->next_pg = blk->pages;
+
+        blk->data = g_malloc0(volt_add_mem(geo->pg_size * geo->pg_per_blk));
+        if (!blk->data)
+            return VOLT_MEM_ERROR;
+
+        VoltPage *pg = blk->pages;
+        for (i_pg = 0; i_pg < geo->pg_per_blk; i_pg++) {
+            pg = volt_init_page(pg, page_count);
+            page_count++;
+        }
     }
+    return page_count;
 }
 
-static LnvmVoltBlock *nvme_volt_get_block(LnvmVoltCtrl *volt, struct ppa_addr addr){
-    return volt->luns[addr.g.lun].blk_offset+addr.g.blk;
+static int volt_init_luns(void)
+{
+    int i_lun;
+    struct nvm_mmgr_geometry *geo = volt_mmgr.geometry;
+    
+    volt->luns = g_malloc(volt_add_mem(sizeof (VoltLun) * geo->lun_per_ch));
+    if (!volt->luns)
+        return VOLT_MEM_ERROR;
+
+    for (i_lun = 0; i_lun < geo->lun_per_ch; i_lun++) {
+        volt->luns[i_lun].blk_offset = &volt->blocks[i_lun * geo->blk_per_lun];
+    }
+    return VOLT_MEM_OK;
 }
 
-static int nvme_volt_print_read(NvmeRequest *req, struct ppa_addr addr, QEMUIOVector *iov){
-    LnvmVoltCtrl *volt = (LnvmVoltCtrl *) req->ns->ctrl->volt_ctrl;
+static int volt_init_channels(void)
+{
+    int i_ch;
+    struct nvm_mmgr_geometry *geo = volt_mmgr.geometry;
+    
+    volt->channels = g_malloc(volt_add_mem(sizeof (VoltCh) * geo->n_of_ch));
+    if (!volt->channels)
+        return VOLT_MEM_ERROR;
+
+    for (i_ch = 0; i_ch < geo->n_of_ch; i_ch++) {
+        volt->channels[i_ch].lun_offset = &volt->luns[i_ch * geo->lun_per_ch];
+    }
+    return VOLT_MEM_OK;
+}
+
+static void volt_clean_mem(void)
+{
+    struct nvm_mmgr_geometry *geo = volt_mmgr.geometry;
+    
+    int total_blk = geo->blk_per_lun * geo->lun_per_ch;
+    
+    int i;
+    for (i = 0; i < total_blk; i++) {
+        g_free(volt->blocks[i].data);
+        volt_sub_mem(geo->pg_size * geo->pg_per_blk);
+        
+        g_free(volt->blocks[i].pages);
+        volt_sub_mem(sizeof (VoltPage) * geo->pg_per_blk);
+        
+    }
+    g_free(volt->blocks);
+    volt_sub_mem(sizeof (VoltBlock) * total_blk);
+
+    g_free(volt->luns);
+    volt_sub_mem(sizeof (VoltLun) * geo->lun_per_ch);
+}
+
+static void volt_callback (struct nvm_mmgr_io_cmd *cmd)
+{
+    
+}
+
+static int volt_process_io (struct nvm_mmgr_io_cmd *cmd)
+{
+    printf("Volt got an IO: %x\n", cmd->cmdtype);
+    return 0;
+}
+
+static void *volt_io_thread (void *arg)
+{
+    printf("Thread Volt!");
+    
+    struct nvm_mmgr_io_cmd *cmd;
+    uint64_t *cmd_addr;
+    int ret;
+    
+    volt->status.active = 1;
+    do {
+        cmd = 0;
+        ret = mq_receive (volt->mq_id, (char *)&cmd_addr,
+                                                      sizeof (uint64_t), NULL);
+        if (ret != VOLT_MQ_MSGSIZE)
+            continue;
+
+        cmd = (struct nvm_mmgr_io_cmd *) cmd_addr;
+	if(cmd == NULL)
+            continue;
+
+        ret = volt_process_io(cmd);
+
+        if (ret) {
+            log_err ("[ERROR: Cmd %x not completed. Aborted.]\n", cmd->cmdtype);
+            /* Callback error */
+            continue;
+        }
+        
+        /* Callback success */
+
+    } while (volt->status.active);
+}
+
+static int volt_init(void)
+{  
+    int pages_ok;
+    int res_l;
+    int res_c;
+    
+    volt = malloc (sizeof (VoltCtrl));
+    if (!volt) 
+        return -1;
+    
+    volt->status.allocated_memory = 0;
+
+    /* Memory allocation. For now only LUNs, blocks and pages */
+    pages_ok = volt_init_blocks();
+    res_l = volt_init_luns();
+    res_c = volt_init_channels();
+
+    if (!pages_ok || !res_l || !res_c)
+        goto MEM_CLEAN;
+    
+    mq_unlink ("/volt");
+    struct mq_attr mqAttr = {0, VOLT_MQ_MAXMSG, VOLT_MQ_MSGSIZE, 0};
+    volt->mq_id = mq_open ("/volt", O_RDWR|O_CREAT, S_IWUSR|S_IRUSR, &mqAttr);
+    if (volt->mq_id < 0)
+	return -1;
+
+    if(pthread_create(&volt->io_thread, NULL, volt_io_thread, NULL))
+        return -1;
+        
+    volt->status.ready = 1; /* ready to use */
+        
+    printf(" [volt: Volatile memory usage: %lu Mb]\n", 
+                                      volt->status.allocated_memory / 1048576);
+        
+MEM_CLEAN:
+    volt->status.ready = 0;
+    volt_clean_mem();
+    printf(" [volt: Not initialized! Memory allocation failed.]\n");
+    printf(" [volt: Volatile memory usage: %lu bytes.]\n", 
+                                                volt->status.allocated_memory);
+}
+
+static int volt_read_page (struct nvm_mmgr_io_cmd *cmd_nvm)
+{
     printf("Volt device is readind!\n");
 
     // VERIFY ADDRESS
     // VERIFY PAGE STATE
 
-    LnvmVoltBlock *blk = nvme_volt_get_block(volt, addr);
-    memcpy(iov->iov->iov_base,&blk->data[addr.g.pg * volt->params.pg_size],iov->iov->iov_len);
-    printf("15 first chars read: %.15s\n",(char *)iov->iov->iov_base);
+    VoltBlock *blk = volt_get_block(addr);
+    memcpy(/*prp*/,&blk->data[addr.g.pg * volt_mmgr.geometry->pg_size],/*len*/);
+    
+    printf("15 first chars read: %.15s\n",(char *)/*prp*/);
 
     return 0;
 }
 
-static int nvme_volt_print_write(NvmeRequest *req, struct ppa_addr addr, QEMUIOVector *iov){
-    LnvmVoltCtrl *volt = (LnvmVoltCtrl *) req->ns->ctrl->volt_ctrl;
+static int volt_write_page (struct nvm_mmgr_io_cmd *cmd_nvm)
+{
     printf("Volt device is writing!\n");
 
     // VERIFY ADDRESS
@@ -43,195 +231,13 @@ static int nvme_volt_print_write(NvmeRequest *req, struct ppa_addr addr, QEMUIOV
     // MODIFY PAGE STATE
     // MODIFY NEXT PAGE
 
-    LnvmVoltBlock *blk = nvme_volt_get_block(volt, addr);
-    memcpy(&blk->data[addr.g.pg * volt->params.pg_size],iov->iov->iov_base,iov->iov->iov_len);
-    printf("15 first chars written: %.15s\n",(char *)&blk->data[addr.g.pg * volt->params.pg_size]);
+    VoltBlock *blk = volt_get_block(addr);
+    memcpy(&blk->data[addr.g.pg * volt_mmgr.geometry->pg_size],/*prp*/,/*len*/);
+    
+    printf("15 first chars written: %.15s\n",
+                   (char *)&blk->data[addr.g.pg * volt_mmgr.geometry->pg_size]);
 
     return 0;
-}
-
-static void nvme_volt_bh(void *opaque)
-{
-    LnvmVoltBlockAIOCBCoroutine *acb = opaque;
-
-    acb->common.cb(acb->common.opaque, acb->req.error);
-
-    qemu_bh_delete(acb->bh);
-    qemu_aio_unref(acb);
-}
-
-void coroutine_fn nvme_volt_redirect_co(void *opaque){
-    LnvmVoltBlockAIOCBCoroutine *acb = opaque;
-    BlockDriverState *bs = acb->common.bs;
-    NvmeRequest *req = (NvmeRequest *) ((LnvmVoltBlockAIOCBCoroutine *) acb->common.opaque)->common.opaque;
-
-    struct ppa_addr lnvm_addr;
-    lnvm_addr.ppa = acb->req.sector;
-    printf("LightNVM address: %#018lx\n", lnvm_addr.ppa);
-    printf("LightNVM address: blk:0x%04x, pg:0x%04x, sec:0x%02x, pl:0x%02x, lun:0x%02x, ch:0x%02x\n",
-            lnvm_addr.g.blk, lnvm_addr.g.pg, lnvm_addr.g.sec, lnvm_addr.g.pl, lnvm_addr.g.lun, lnvm_addr.g.ch);
-
-    if (!acb->is_write) {
-        acb->req.error = nvme_volt_print_read(req, lnvm_addr, acb->req.qiov); // VOLT READ
-    } else {
-        acb->req.error = nvme_volt_print_write(req, lnvm_addr, acb->req.qiov); // VOLT WRITE
-    }
-
-    acb->bh = aio_bh_new(bdrv_get_aio_context(bs), nvme_volt_bh, acb);
-    qemu_bh_schedule(acb->bh);
-}
-
-static const AIOCBInfo nvme_volt_aiocb_info = {
-    .aiocb_size         = sizeof(LnvmVoltBlockAIOCBCoroutine),
-};
-
-static BlockAIOCB *nvme_volt_redirect_rw(BlockDriverState *bs,
-                                         int64_t sector_num,
-                                         QEMUIOVector *qiov,
-                                         int nb_sectors,
-                                         BdrvRequestFlags flags,
-                                         BlockCompletionFunc *cb,
-                                         void *opaque,
-                                         bool is_write){
-    Coroutine *co;
-    LnvmVoltBlockAIOCBCoroutine *acb;
-
-    acb = qemu_aio_get(&nvme_volt_aiocb_info, bs, cb, opaque);
-    acb->req.sector = sector_num;
-    acb->req.nb_sectors = nb_sectors;
-    acb->req.qiov = qiov;
-    acb->req.flags = flags;
-    acb->is_write = is_write;
-
-    co = qemu_coroutine_create(nvme_volt_redirect_co);
-    qemu_coroutine_enter(co, acb);
-
-    return &acb->common;
-}
-
-BlockAIOCB *nvme_volt_redirect_write(BlockBackend *blk, int64_t sector_num,
-                           QEMUIOVector *iov, int nb_sectors,
-                           BlockCompletionFunc *cb, void *opaque){
-    return nvme_volt_redirect_rw(blk->bs, sector_num, iov, nb_sectors, 0, cb, opaque, true);
-}
-
-BlockAIOCB *nvme_volt_redirect_read(BlockBackend *blk, int64_t sector_num,
-                           QEMUIOVector *iov, int nb_sectors,
-                           BlockCompletionFunc *cb, void *opaque){
-    return nvme_volt_redirect_rw(blk->bs, sector_num, iov, nb_sectors, 0, cb, opaque, false);
-}
-
-static size_t nvme_volt_add_mem(LnvmVoltCtrl *volt, int64_t bytes)
-{
-    volt->status.allocated_memory += bytes;
-    return bytes;
-}
-
-static void nvme_volt_sub_mem(LnvmVoltCtrl *volt, int64_t bytes)
-{
-    volt->status.allocated_memory -= bytes;
-}
-
-static LnvmVoltPage * nvme_volt_init_page(LnvmVoltPage *pg, uint16_t pg_id)
-{
-    pg->state = 0;
-    return ++pg;
-}
-
-static int nvme_volt_init_blocks(LnvmVoltCtrl *volt)
-{
-    int page_count = 0;
-    int i_blk;
-    int i_pg;
-    int total_blk = volt->params.num_blk * volt->params.num_lun;
-
-    volt->blocks = g_malloc(nvme_volt_add_mem(volt, sizeof(LnvmVoltBlock) * total_blk));
-    if (!volt->blocks)
-        return LNVM_VOLT_MEM_ERROR;
-               
-    for (i_blk = 0; i_blk < total_blk; i_blk++) {
-        LnvmVoltBlock *blk = &volt->blocks[i_blk];
-        blk->id = i_blk;
-        blk->life = LNVM_VOLT_BLK_LIFE;
-
-        blk->pages = g_malloc(nvme_volt_add_mem(volt, sizeof(LnvmVoltPage) * volt->params.num_pg));
-        if (!blk->pages)
-            return LNVM_VOLT_MEM_ERROR;
-        blk->next_pg = blk->pages;
-
-        blk->data = g_malloc0(nvme_volt_add_mem(volt, volt->params.pg_size * volt->params.num_pg));
-        if (!blk->data)
-            return LNVM_VOLT_MEM_ERROR;
-
-        LnvmVoltPage *pg = blk->pages;
-        for (i_pg = 0; i_pg < volt->params.num_pg; i_pg++) {
-            pg = nvme_volt_init_page(pg, page_count);
-            page_count++;
-        }
-    }
-    return page_count;
-}
-
-static int nvme_volt_init_luns(LnvmVoltCtrl *volt)
-{
-    int i_lun;
-    volt->luns = g_malloc(nvme_volt_add_mem(volt, sizeof (LnvmVoltLun) * volt->params.num_lun));
-    if (!volt->luns)
-        return LNVM_VOLT_MEM_ERROR;
-
-    for (i_lun = 0; i_lun < volt->params.num_lun; i_lun++) {
-        volt->luns[i_lun].blk_offset = &volt->blocks[i_lun * volt->params.num_blk];
-    }
-    return LNVM_VOLT_MEM_OK;
-}
-
-static void nvme_volt_clean_mem(LnvmVoltCtrl *volt)
-{
-    int total_blk = volt->params.num_blk * volt->params.num_lun;
-    int i;
-    for (i = 0; i < total_blk; i++) {
-        g_free(volt->blocks[i].data);
-        nvme_volt_sub_mem(volt, volt->params.pg_size * volt->params.num_pg);
-        g_free(volt->blocks[i].pages);
-        nvme_volt_sub_mem(volt, sizeof (LnvmVoltPage) * volt->params.num_pg);
-    }
-    g_free(volt->blocks);
-    nvme_volt_sub_mem(volt, sizeof (LnvmVoltBlock) * total_blk);
-
-    g_free(volt->luns);
-    nvme_volt_sub_mem(volt, sizeof (LnvmVoltLun) * volt->params.num_lun);
-}
-
-static void volt_init(void)
-{  
-    volt->status.allocated_memory = 0;
-
-    /* Memory allocation. For now only LUNs, blocks and pages */
-    int pages_ok = nvme_volt_init_blocks(volt);
-    int res = nvme_volt_init_luns(volt);
-
-    if (!pages_ok || !res)
-        goto MEM_CLEAN;        
-        
-    volt->status.ready = 1; /* ready to use */
-        
-    printf("volt: Volatile memory usage: %lu Mb\n", volt->status.allocated_memory / 1048576);
-        
-MEM_CLEAN:
-    volt->status.ready = 0;
-    nvme_volt_clean_mem(volt);
-    printf("volt: Not initialized ns%d! Memory allocation failed.\n", volt->ns->id);
-    printf("volt: Volatile memory usage: %lu bytes.\n", volt->status.allocated_memory);
-}
-
-static int volt_read_page (struct nvm_mmgr_io_cmd *cmd_nvm)
-{
-    
-}
-
-static int volt_write_page (struct nvm_mmgr_io_cmd *cmd_nvm)
-{
-    
 }
 
 static int volt_erase_blk (struct nvm_mmgr_io_cmd *cmd_nvm)
@@ -241,7 +247,10 @@ static int volt_erase_blk (struct nvm_mmgr_io_cmd *cmd_nvm)
 
 static void volt_exit (struct nvm_mmgr *mmgr)
 {
-    
+    volt_clean_mem();
+    volt->status.active = 0;
+    mq_close(volt->mq_id);
+    free (volt);
 }
 
 static int volt_set_ch_info (struct nvm_channel *ch, uint16_t nc)
@@ -278,17 +287,15 @@ int mmgr_volt_init(void)
 {
     int ret = 0;
 
-    dfcnand.name     = "VOLT";
-    dfcnand.ops      = &volt_ops;
-    dfcnand.geometry = &volt_geo;
+    volt_mmgr.name     = "VOLT";
+    volt_mmgr.ops      = &volt_ops;
+    volt_mmgr.geometry = &volt_geo;
 
     ret = volt_init();
     if(ret) {
-        syslog(LOG_ERR, "volt: Not possible to start VOLT.");
+        log_err(LOG_ERR, "volt: Not possible to start VOLT.");
         return -1;
     }
-    //ret = dfcnand_start_prp_map();
-    //if (ret) return ret;
 
     return nvm_register_mmgr(&volt);
 }
--- hw/block/ox-ctrl/mmgr/volt/volt.h
+++ hw/block/ox-ctrl/mmgr/volt/volt.h
@@ -2,16 +2,11 @@
 #define VOLT_SSD_H
 
 #include <stdint.h>
-#include "include/qemu/timer.h"
-#include "block/coroutine.h"
-#include "block/aio.h"
-#include "block/block.h"
-#include "block/block_int.h"
-#include "sysemu/block-backend.h"
+#include <pthread.h>
 
-#define LNVM_VOLT_MEM_ERROR 0
-#define LNVM_VOLT_MEM_OK 1
-#define LNVM_VOLT_SECOND 1000000 /* from u-seconds */
+#define VOLT_MEM_ERROR 0
+#define VOLT_MEM_OK 1
+#define VOLT_SECOND 1000000 /* from u-seconds */
 
 #define VOLT_CHIP_COUNT     = 8
 #define VOLT_VIRTUAL_LUNS   = 4
@@ -24,10 +19,14 @@
 #define VOLT_OOB_SIZE       = 0x400
 
 /* should be user-defined */
-#define LNVM_VOLT_BLK_LIFE 5000
+#define VOLT_BLK_LIFE 5000
+
+#define VOLT_MQ_MAXMSG      64
+#define VOLT_MQ_MSGSIZE     8
 
 typedef struct VoltStatus {
     uint8_t     ready; /* 0x00-busy, 0x01-ready to use */
+    uint8_t     active;
     int64_t     allocated_memory;
 } VoltStatus;
 
@@ -47,10 +46,17 @@ typedef struct VoltLun {
     VoltBlock       *blk_offset;
 } VoltLun;
 
+typedef struct VoltCh {
+    VoltLun         *lun_offset;
+};
+
 typedef struct VoltCtrl {
     VoltStatus      status;
     VoltBlock       *blocks;
     VoltLun         *luns;
-} LnvmVoltCtrl;
+    VoltCh          *channels;
+    uint16_t        mq_id;
+    pthread_t       io_thread;
+} VoltCtrl;
 
 #endif /* VOLT_SSD_H */ 
