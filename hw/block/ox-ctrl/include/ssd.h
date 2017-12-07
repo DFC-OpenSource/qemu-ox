1e1ecd26f6e84af400d43e833be1b38b3f13d278
ivpi <ivpi@itu.dk>
2017-12-07 20:07:54 +0700

    lightnvm: roolback to generic PPA bits

--- hw/block/ox-ctrl/include/ssd.h
+++ hw/block/ox-ctrl/include/ssd.h
@@ -80,13 +80,12 @@ struct nvm_ppa_addr {
     /* Generic structure for all addresses */
     union {
         struct {
-            uint64_t sec   : 2;
-            uint64_t pl    : 1;
-            uint64_t ch    : 3;
-            uint64_t lun   : 2;
-            uint64_t pg    : 7;
-            uint64_t blk   : 5;
-            uint64_t rsv   : 44;
+            uint64_t sec   : 8;
+            uint64_t pl    : 8;
+            uint64_t ch    : 8;
+            uint64_t lun   : 8;
+            uint64_t pg    : 16;
+            uint64_t blk   : 16;
         } g;
 
         uint64_t ppa;
